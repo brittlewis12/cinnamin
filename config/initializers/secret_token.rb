4819aa78ae67bdc44171070b61b72875c1a0c372
brittlewis12 <brittlewis12@gmail.com>
2013-12-16 13:35:51 -0500

    initial commit, rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cinnamin::Application.config.secret_key_base = '0c860e66c35757f54824c91ab937ef11a46773d1c8fa9ed92f21c2246b2b7635eae008bdf38e68571accad01a312eaa1f1abe64097892cf0475a676cde51da14'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cinnamin::Application.config.session_store :cookie_store, key: '_cinnamin_session'
